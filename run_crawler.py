572a8c911cd22c2834ecf6d83a253f331f730f32
RushFTK <867871766@qq.com>
2017-12-23 18:51:28 +0800

    update do_crawler_x86 scrpit.

--- run_crawler.py
+++ run_crawler.py
@@ -1,11 +1,11 @@
 import os
-import commands
+import subprocess
 from test_windowssupport import *
 (perp_state,prep_reason) = environment_tester()
 location = os.path.dirname(os.path.realpath(__file__))
-exec_order = 'cd' + location +  '\crawler_scrapy & scrapy crawl test'
-# output1 = os.popen(exec_order)
-# print output1.read()
-(status, output) = commands.getstatusoutput(exec_order)
-print status, output
+exec_order = 'cd ' + location +  '\crawler_scrapy & scrapy crawl test'
+# exec_order = 'cd ' + location +  '\crawler_scrapy & dir'
+print exec_order
+result = subprocess.call(exec_order,shell=True);
+print result
 
